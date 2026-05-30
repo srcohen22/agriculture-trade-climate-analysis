8757acba91cfb49b2bf7251825e5bb8f0b3e7cf7
srcohen22 <samuelrcohen18@gmail.com>
2026-05-30 18:15:22 -0400

    Filtered out useful columns from trade data

--- scripts/process_trade_data.py
+++ scripts/process_trade_data.py
@@ -1,7 +1,24 @@
 import pandas as pd
 
 # Load the trade data into a data frame
-trade_data = pd.read_csv('data/TradeData_5_26_2026_19_8_1.csv', encoding="iso-8859-1")
+trade_data = pd.read_csv('data/TradeData_5_26_2026_19_8_1.csv', encoding="iso-8859-1", index_col=False)
 
-# Print out basic information
-print(trade_data.info())
+# Useful colums:
+#   - refPeriodId: year for data
+#   - reporterCode: country code
+#   - reporterISO: country name
+#   - flowCode: import/export id
+#   - flowDesc: import/export name
+#   - partnerCode: partner country id
+#   - partnerISO: partner country name
+#   - cmdCode: product code
+#   - cmdDesc: product name
+#   - qtyUnitCode: code for quantity unit
+#   - qtyUnitAbbr: name of quantity unit
+#   - qty: quantity of trade
+useful_columns = ["refYear", "reporterCode", "reporterISO", "flowCode", "flowDesc", "partnerCode", "partnerISO", "cmdCode", "cmdDesc", "qtyUnitCode", "qtyUnitAbbr", "qty"]
+filtered_trade_data = trade_data[useful_columns]
+
+# Print out info
+print(filtered_trade_data.info())
+print(filtered_trade_data.head())
